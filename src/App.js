2b31291bed1a6bdb861bd0e055249737c61e890f
Inmobi Korea <taki1984@naver.com>
2019-08-08 15:02:10 +0900

    2.3 map Recap

--- src/App.js
+++ src/App.js
@@ -2,26 +2,31 @@ import React from 'react';
 
 const food_list = [
   {
+    id: 1,
     name: "kimchi",
     image:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGGj53NQnIIR8qujVgjoEB9f9-JufDEhE_mwDVmin4CjBvxHQcZQ"
   },
   {
+    id: 2,
     name: "Pork Valley",
     image:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQG9FYqNhMdYZ4-KkJB46IcuEoZClV_ijBzeqoBtRGe-Z0qTuajAw"
   },
   {
+    id: 3,
     name: "rice",
     image:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_FPW5ZK51Wjgt7bYymze6qVONcnmGX80IW43-M4LWugOBQNB3"
   },
   {
+    id: 4,
     name: "steak",
     image:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQx21tYkGvec4BA4MwN-u520cy-78hH91R6HFzqLZcIwisSeS2n"
   },
   {
+    id: 5,
     name: "soup",
     image:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTb5UgoNJ9ttLCw_kGSXanNPuLt-UD_rMTqHTCL5sGjNqw4Honk"
@@ -30,10 +35,10 @@ const food_list = [
 
 function Food({name, image}){
   return (
-    <>
-    <h1>I like {name}</h1>
-    <img src={image} alt={name}/>
-    </>
+    <div>
+      <h1>I like {name}</h1>
+      <img src={image} alt={name}/>
+    </div>
   );
 }
 
@@ -41,7 +46,10 @@ function App() {
   return (
     <div>
       {/* {food_list.map((food, index) => <Food name={food.name} image={food.image}  key={index}></Food>)} */}
-      {food_list.map((food, index) => <Food {...food} key={index}></Food>)}
+      {/* {food_list.map((food, index) => <Food {...food} key={index}></Food>)} */}
+      {food_list.map(food => (
+        <Food {...food} key={food.id} />
+      ))}
     </div>
   );
 }
