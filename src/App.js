06327012f0e6c5cdbfc792bfece4497ead3a00dc
Inmobi Korea <taki1984@naver.com>
2019-08-08 15:07:14 +0900

    extra. prop-types installed

--- src/App.js
+++ src/App.js
@@ -5,31 +5,36 @@ const food_list = [
     id: 1,
     name: "kimchi",
     image:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGGj53NQnIIR8qujVgjoEB9f9-JufDEhE_mwDVmin4CjBvxHQcZQ"
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGGj53NQnIIR8qujVgjoEB9f9-JufDEhE_mwDVmin4CjBvxHQcZQ",
+    rating: 5.0
   },
   {
     id: 2,
     name: "Pork Valley",
     image:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQG9FYqNhMdYZ4-KkJB46IcuEoZClV_ijBzeqoBtRGe-Z0qTuajAw"
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQG9FYqNhMdYZ4-KkJB46IcuEoZClV_ijBzeqoBtRGe-Z0qTuajAw",
+    rating: 4.4
   },
   {
     id: 3,
     name: "rice",
     image:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_FPW5ZK51Wjgt7bYymze6qVONcnmGX80IW43-M4LWugOBQNB3"
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_FPW5ZK51Wjgt7bYymze6qVONcnmGX80IW43-M4LWugOBQNB3",
+    rating: 3.8
   },
   {
     id: 4,
     name: "steak",
     image:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQx21tYkGvec4BA4MwN-u520cy-78hH91R6HFzqLZcIwisSeS2n"
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQx21tYkGvec4BA4MwN-u520cy-78hH91R6HFzqLZcIwisSeS2n",
+    rating: 5.0
   },
   {
     id: 5,
     name: "soup",
     image:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTb5UgoNJ9ttLCw_kGSXanNPuLt-UD_rMTqHTCL5sGjNqw4Honk"
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTb5UgoNJ9ttLCw_kGSXanNPuLt-UD_rMTqHTCL5sGjNqw4Honk",
+    rating: 3.2
   }
 ];
 
