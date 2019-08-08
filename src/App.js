c52a48fce4a94af3352a8d3081a5e32a7f4d8653
Inmobi Korea <taki1984@naver.com>
2019-08-08 11:33:13 +0900

    2.2 Dynamic Component Generation

--- src/App.js
+++ src/App.js
@@ -1,17 +1,47 @@
 import React from 'react';
 
-function Food({ favorite }){
-  return <h1>I like {favorite}</h1>;
+const food_list = [
+  {
+    name: "kimchi",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGGj53NQnIIR8qujVgjoEB9f9-JufDEhE_mwDVmin4CjBvxHQcZQ"
+  },
+  {
+    name: "Pork Valley",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQG9FYqNhMdYZ4-KkJB46IcuEoZClV_ijBzeqoBtRGe-Z0qTuajAw"
+  },
+  {
+    name: "rice",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_FPW5ZK51Wjgt7bYymze6qVONcnmGX80IW43-M4LWugOBQNB3"
+  },
+  {
+    name: "steak",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQx21tYkGvec4BA4MwN-u520cy-78hH91R6HFzqLZcIwisSeS2n"
+  },
+  {
+    name: "soup",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTb5UgoNJ9ttLCw_kGSXanNPuLt-UD_rMTqHTCL5sGjNqw4Honk"
+  }
+];
+
+function Food({name, image}){
+  return (
+    <>
+    <h1>I like {name}</h1>
+    <img src={image} alt={name}/>
+    </>
+  );
 }
 
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
-      <Food favorite="kimchi" />
-      <Food favorite="rice" />
-      <Food favorite="pork valley" />
-      <Food favorite="steak" />
+      {/* {food_list.map((food, index) => <Food name={food.name} image={food.image}  key={index}></Food>)} */}
+      {food_list.map((food, index) => <Food {...food} key={index}></Food>)}
     </div>
   );
 }
